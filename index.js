b77a0a06deb90246498599cd66b47f8f63035f4b
grabermtw <grabermtw@gmail.com>
2019-04-11 11:34:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -346,6 +346,6 @@ app.post('/api/create', function(req, res) {
 });
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port 3000!');
 });
